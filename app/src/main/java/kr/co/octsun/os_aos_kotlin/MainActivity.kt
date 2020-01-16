649a74a7cc418e86a4c720eb9f7ab5f3462c5cec
hskwon <OctSun89@gmail.com>
2020-01-16 17:56:03 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/octsun/os_aos_kotlin/MainActivity.kt
@@ -0,0 +1,12 @@
+package kr.co.octsun.os_aos_kotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
